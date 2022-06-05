15067c9f0e818b4c41fa2463e66002b44dc143fc
Duncan P <duncan@boldorion.com>
2022-06-05 17:20:17 -0400

    Added updater and extenter

--- functions.php
+++ functions.php
@@ -10,14 +10,57 @@
  *
  * @return void
  */
+ 
+/**
+ * Let's pull the child theme styles
+ */
+$orionv = "1.0.2"; 
+ 
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
-		'hello-elementor-child-style',
+		'orion-base',
 		get_stylesheet_directory_uri() . '/style.css',
 		[
-			'hello-elementor-theme-style',
+			'orion-base',
 		],
-		'1.0.0'
+		$orionv
 	);
+	
+   // Style extensions
+  if (file_exists(get_stylesheet_directory_uri() . '/css/style.css'))
+  {
+	wp_enqueue_style(
+		'orion-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'orion-style',
+		],
+		$orionv
+	);
+  }
+	
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+/**
+ * Check for updates from Github to keep the core up to date.
+ */
+ 
+require 'updater/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/boldorion/wpt-orion/',
+	__FILE__,
+	'orion'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
+
+/**
+ * Let's see if any theme customizations exist, and pull them in.
+ */
+ 
+ // Functions extensions
+ if (file_exists(get_stylesheet_directory_uri() . '/extended.php'));
+  include (get_stylesheet_directory_uri() . '/extended.php');
+  
